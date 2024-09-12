091e9f5e3a293ec0b73668147d6051a25bd0b581
Marcus Quach <marcusquach07@gmail.com>
2024-09-12 12:41:56 -0600

    Done

--- test_math_it_up.py
+++ test_math_it_up.py
@@ -31,23 +31,51 @@ def test_is_even():
   """
   Tests for the `is_even` function
   """
+  assert math_it_up.is_even(4) == True
+  assert math_it_up.is_even(-2) == True
+  assert math_it_up.is_even(1) != True
+  assert math_it_up.is_even(-1004) == True
+  assert math_it_up.is_even(0) == True
+  assert math_it_up.is_even(-3) == False
 
 def test_is_odd():
   """
   Tests for the `is_odd` function
   """
+  assert math_it_up.is_odd(1) == True
+  assert math_it_up.is_odd(23) == True
+  assert math_it_up.is_odd(24) == False
+  assert math_it_up.is_odd(7) != False
+  assert math_it_up.is_odd(9) == True
+  assert math_it_up.is_odd(12) == False
+  assert math_it_up.is_odd(5) == True
 
 def test_mean():
   """
   Tests for the `mean` function
   """
+  assert math_it_up.mean([1, 2, 2, 4, 5, 0, 0]) == 2
+  assert math_it_up.mean([2, 2, 0, 0]) == 1
+  assert math_it_up.mean([0, 0, 0, 0, 0, 0, 0, 0]) == 0
+  assert math_it_up.mean([-10, -20, -30]) == -20
+  assert math_it_up.mean([6]) == 6
 
 def test_median():
   """
   Tests for the `median` function
   """
+  assert math_it_up.median([0, 2, 2, 3, 3]) == 2
+  assert math_it_up.median([23, 24, 25, 34, 39]) == 25
+  assert math_it_up.median([0, -2, -2, -3, -3]) == -2
+  assert math_it_up.median([0, 12, 22, 33, 43]) == 22
+  assert math_it_up.median([0, 42, 68, 73, 133]) == 68
 
 def test_mode():
   """
   Tests for the `mode` function
   """
+  assert math_it_up.mode([2, 2, 2, 2, 3, 4]) == [2]
+  assert math_it_up.mode([-5, -5, -5, -5, -7, -8, -10, -14, -20]) == [-5]
+  assert math_it_up.mode([16, 24, 30, 44, 65, 65, 65, 65, 80]) == [65]
+  assert math_it_up.mode([9]) == [9]
+  assert math_it_up.mode([0, 0, 0, 0, 0, 0, 0, 0, 0]) == [0]
